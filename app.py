b2080cd59fc5aa2f3484abb1d023fc354d305675
aliesl1418 <149317587+aliesl1418@users.noreply.github.com>
2024-01-09 18:29:09 +0330

    updated

--- app.py
+++ app.py
@@ -17,73 +17,73 @@ def home():
     return render_template("login.html")
 
 
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    message = ""
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        status, data = ProfileController.login(username, password)
-        if status:
-            session["username"] = username
-            return render_template("profile.html", profile=data)
-        else:
-            message = data
-    return render_template("login.html", message=message)
+# @app.route("/login", methods=["POST", "GET"])
+# def login():
+#     message = ""
+#     if request.method == "POST":
+#         username = request.form.get("username")
+#         password = request.form.get("password")
+#         status, data = ProfileController.login(username, password)
+#         if status:
+#             session["username"] = username
+#             return render_template("profile.html", profile=data)
+#         else:
+#             message = data
+#     return render_template("login.html", message=message)
 
 
-@app.route("/profile", methods=["POST", "GET", "DELETE"])
-def profile():
-    if not session.get("username"):
-        return render_template("login.html")
+# @app.route("/profile", methods=["POST", "GET", "DELETE"])
+# def profile():
+#     if not session.get("username"):
+#         return render_template("login.html")
+#
+#     if request.method == "POST":
+#         name = request.form.get("name")
+#         family = request.form.get("family")
+#         username = request.form.get("username")
+#         password = request.form.get("password")
+#         status, data = ProfileController.save(name, family, username, password)
+#     elif request.method == "DELETE":
+#         ProfileController.remove(request.args.get("id"))
+#
+#     # return data, 204
+#     return render_template("profile.html", profile=ProfileController.find_by_username(session.get("username"))[1])
+#
 
-    if request.method == "POST":
-        name = request.form.get("name")
-        family = request.form.get("family")
-        username = request.form.get("username")
-        password = request.form.get("password")
-        status, data = ProfileController.save(name, family, username, password)
-    elif request.method == "DELETE":
-        ProfileController.remove(request.args.get("id"))
-
-    # return data, 204
-    return render_template("profile.html", profile=ProfileController.find_by_username(session.get("username"))[1])
-
-
-@app.route("/register", methods=["POST", "GET"])
-def register():
-    if request.method == "POST":
-        # if request.form.get("password") == request.form.get("repeat_password"):
-
-        status, data = ProfileController.save(
-            request.form.get("name"),
-            request.form.get("family"),
-            request.form.get("email"),
-            request.form.get("password"))
-        return render_template("login.html")
-
-
-    return render_template("register.html")
-
-
-@app.route("/post", methods=["POST", "GET"])
-def post():
-    if not session.get("username"):
-        return render_template("login.html")
-    if request.method == "POST":
-        pass
-    return render_template("post.html", posts=ProfileController.find_by_username(session.get("username"))[1].posts)
+# @app.route("/register", methods=["POST", "GET"])
+# def register():
+#     if request.method == "POST":
+#         # if request.form.get("password") == request.form.get("repeat_password"):
+#
+#         status, data = ProfileController.save(
+#             request.form.get("name"),
+#             request.form.get("family"),
+#             request.form.get("email"),
+#             request.form.get("password"))
+#         return render_template("login.html")
+#
+#
+#     return render_template("register.html")
 
 
-@app.route("/forget")
-def forget():
-    return render_template("forget-password.html")
-
-
-@app.route("/logout")
-def logout():
-    session["username"] = None
-    return redirect("/")
+# @app.route("/post", methods=["POST", "GET"])
+# def post():
+#     if not session.get("username"):
+#         return render_template("login.html")
+#     if request.method == "POST":
+#         pass
+#     return render_template("post.html", posts=ProfileController.find_by_username(session.get("username"))[1].posts)
+#
+#
+# @app.route("/forget")
+# def forget():
+#     return render_template("forget-password.html")
+#
+#
+# @app.route("/logout")
+# def logout():
+#     session["username"] = None
+#     return redirect("/")
 
 
 
